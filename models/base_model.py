08282007b93495607afb99557ad4746473dd4188
maitha1-nule <nuleahmed@gmail.com>
2024-02-12 14:30:35 -0800

    In this task i have changed the dunder init file making sure that the script willbe able to store the files in my attribute

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,19 @@ class BaseModel:
     This is the parent class of the entire program
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Dunder init constructor for all the functions"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -43,6 +51,6 @@ class BaseModel:
         """
         inst_dict = self.__dict__.copy()
         inst_dict['__class__'] = self.__clas__.__main__
-        inst_dict[created_at] = self.created_at.isoformat()
-        inst_dict[updated_at] = self.updated_at.isoformat()
+        inst_dict['created_at'] = self.created_at.isoformat()
+        inst_dict['updated_at'] = self.updated_at.isoformat()
         return inst_dict
